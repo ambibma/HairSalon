4f214f9119caa56808faaeb188397e016897b59e
ambi hidalgo <ambio.pk@gmail.com>
2023-03-09 20:54:37 -0800

    Add PhoneNumber to Clients Models and for display

--- Models/Client.cs
+++ Models/Client.cs
@@ -5,8 +5,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId{get; set;}
-    public string Name {get;set;}
     public int StylistId {get;set;}
+    public string Name {get;set;}
+    public string PhoneNumber{get; set;}
 
     public Stylist Stylist {get; set;}
   }
