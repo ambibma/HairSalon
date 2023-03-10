0dd41332a62034fdf4970693baf0dce675b30e6a
ambi hidalgo <ambio.pk@gmail.com>
2023-03-09 18:45:29 -0800

    Add Client Edit functionality

--- Models/Client.cs
+++ Models/Client.cs
@@ -5,9 +5,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId{get; set;}
+    public string Name {get;set;}
     public int StylistId {get;set;}
 
     public Stylist Stylist {get; set;}
-    public string Name {get;set;}
   }
 }
